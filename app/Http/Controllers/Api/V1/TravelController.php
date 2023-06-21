69067289f5402dd1b56349b35967d8ab6ad947e3
rhonstratos <53313285+rhonstratos@users.noreply.github.com>
2023-06-21 14:04:36 +0800

    refactor

--- app/Http/Controllers/Api/V1/TravelController.php
+++ app/Http/Controllers/Api/V1/TravelController.php
@@ -10,45 +10,10 @@ use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 
 class TravelController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
     public function index(): AnonymousResourceCollection
     {
         return TravelResource::collection(
             Travel::where('is_public', true)->paginate()
         );
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(Travel $travel)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, Travel $travel)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(Travel $travel)
-    {
-        //
     }
 }
